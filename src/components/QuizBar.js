54de3c1cb362cb08b5fae459f3ae7a88a2808d6b
Stefano Corra <stefano.corra@hotmail.com>
2020-06-30 22:48:13 -0400

    clean quizBar

--- src/components/QuizBar.js
+++ src/components/QuizBar.js
@@ -7,14 +7,25 @@ import QuizType from './QuizType';
 
 
 function QuizBar(props){
+
+  const quizArray = [
+    {icon: "dice",type: "Random"},
+    {icon: "file-alt",type: "Regular"},
+    {icon: "dumbbell",type: "Weighted"},
+    {icon: "font",type: "Multi"},
+    ]
+
+    const quizTypes = quizArray.map((qt,i)=>{
+      return(
+        <QuizType key ={i} icon={qt.icon} quizType={qt.type} userChoice={props.userChoice}/>
+      )
+    })
+
     return(
       <div className="quiz-bar">
         <h1>Choose your study type</h1>
         <ul className="nav nav-pills nav-fill">
-          <QuizType icon="dice" quizType="Random" userChoice={props.userChoice}/>
-          <QuizType icon="file-alt" quizType="Regular" userChoice={props.userChoice}/>
-          <QuizType icon="dumbbell" quizType="Weighted" userChoice={props.userChoice}/>
-          <QuizType icon="font" quizType="Multi" userChoice={props.userChoice}/>
+          {quizTypes}
         </ul>
       </div>
 
