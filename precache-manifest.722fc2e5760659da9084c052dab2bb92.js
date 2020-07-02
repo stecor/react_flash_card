ed0d3d5b0bbc9c54fbbe87b17aae145f1728c690
Stefano Corra <stefano.corra@hotmail.com>
2020-07-02 18:33:38 -0400

    Updates

---
+++ precache-manifest.722fc2e5760659da9084c052dab2bb92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bdb20c0f48a52edaa01672152a023f7",
+    "url": "/react_flash_card/index.html"
+  },
+  {
+    "revision": "56e020a09d61e7a260eb",
+    "url": "/react_flash_card/static/css/main.2c88ca67.chunk.css"
+  },
+  {
+    "revision": "54d8c3a879870246ffc7",
+    "url": "/react_flash_card/static/js/2.31d47c8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_flash_card/static/js/2.31d47c8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56e020a09d61e7a260eb",
+    "url": "/react_flash_card/static/js/main.0d4c25f8.chunk.js"
+  },
+  {
+    "revision": "386355da31316ecb2312",
+    "url": "/react_flash_card/static/js/runtime-main.5c4a90e9.js"
+  }
+]);
